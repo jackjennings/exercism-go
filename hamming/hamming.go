44deaad176c9bc238c9e999e3def436f32b6b172
Jack Jennings <jack@standard-library.com>
2018-11-18 11:29:14 -0800

    Add hamming documentation strings

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,3 +1,4 @@
+// Package hamming implements functions for working with hamming scores.
 package hamming
 
 import (
@@ -5,6 +6,8 @@ import (
 	"strings"
 )
 
+// Distance returns the number of nucleotides that differ between two
+// DNA strands.
 func Distance(a, b string) (int, error) {
 	if len(a) != len(b) {
 		return -1, errors.New("DNA strands are of unequal length")
