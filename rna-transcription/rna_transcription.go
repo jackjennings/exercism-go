0e7987229404b072ab1c369e6ced55040cc7f4ea
Jack Jennings <jack@standard-library.com>
2018-11-18 22:23:29 -0800

    Use strings.NewReplacer in rna-transcription

--- rna-transcription/rna_transcription.go
+++ rna-transcription/rna_transcription.go
@@ -3,21 +3,12 @@ package strand
 
 import "strings"
 
-var ComplementTable = map[string]string{
-	"C": "G",
-	"G": "C",
-	"T": "A",
-	"A": "U",
-}
-
 // ToRNA converts a DNS strand into the RNA compliment
 func ToRNA(dna string) string {
-	var nucleotides = strings.Split(dna, "")
-	var complement []string
-
-	for i := 0; i < len(nucleotides); i++ {
-		complement = append(complement, ComplementTable[nucleotides[i]])
-	}
-
-	return strings.Join(complement, "")
+	return strings.NewReplacer(
+		"G", "C",
+		"C", "G",
+		"T", "A",
+		"A", "U",
+	).Replace(dna)
 }
