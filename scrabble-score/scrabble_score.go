f72949014537ce3866a1eb0521cf5bdecbef2baa
Jack Jennings <jack@standard-library.com>
2018-11-28 15:58:35 -0800

    Scrabble score solution

---
+++ scrabble-score/scrabble_score.go
@@ -0,0 +1,44 @@
+// Package scrabble implements functions relating to the board game Scrabble
+package scrabble
+
+import "unicode"
+
+var points = map[rune]int{
+  'a': 1,
+  'b': 3,
+  'c': 3,
+  'd': 2,
+  'e': 1,
+  'f': 4,
+  'g': 2,
+  'h': 4,
+  'i': 1,
+  'j': 8,
+  'k': 5,
+  'l': 1,
+  'm': 3,
+  'n': 1,
+  'o': 1,
+  'p': 3,
+  'q': 10,
+  'r': 1,
+  's': 1,
+  't': 1,
+  'u': 1,
+  'v': 4,
+  'w': 4,
+  'x': 8,
+  'y': 4,
+  'z': 10,
+}
+
+// Score returns the total score of a provided english word
+func Score(word string) int {
+  var total int
+
+  for _, letter := range word {
+    total += points[unicode.ToLower(letter)]
+  }
+
+  return total
+}
