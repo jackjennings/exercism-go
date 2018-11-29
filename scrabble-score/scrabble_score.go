04c9b328b779619a0869295f32b288632db230bd
Jack Jennings <jack@standard-library.com>
2018-11-29 10:59:03 -0800

    Use switch statement in scrabble-score

--- scrabble-score/scrabble_score.go
+++ scrabble-score/scrabble_score.go
@@ -3,42 +3,42 @@ package scrabble
 
 import "unicode"
 
-var points = map[rune]int{
-  'a': 1,
-  'b': 3,
-  'c': 3,
-  'd': 2,
-  'e': 1,
-  'f': 4,
-  'g': 2,
-  'h': 4,
-  'i': 1,
-  'j': 8,
-  'k': 5,
-  'l': 1,
-  'm': 3,
-  'n': 1,
-  'o': 1,
-  'p': 3,
-  'q': 10,
-  'r': 1,
-  's': 1,
-  't': 1,
-  'u': 1,
-  'v': 4,
-  'w': 4,
-  'x': 8,
-  'y': 4,
-  'z': 10,
-}
-
 // Score returns the total score of a provided english word
 func Score(word string) int {
   var total int
 
   for _, letter := range word {
-    total += points[unicode.ToLower(letter)]
+    total += Points(letter)
   }
 
   return total
+}
+
+// Points returns the score of a single letter in the English edition
+// A, E, I, O, U, L, N, R, S, T       1
+// D, G                               2
+// B, C, M, P                         3
+// F, H, V, W, Y                      4
+// K                                  5
+// J, X                               8
+// Q, Z                               10
+func Points(letter rune) int {
+  switch unicode.ToLower(letter) {
+  case 'a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't':
+    return 1
+  case 'd', 'g':
+    return 2
+  case 'b', 'c', 'm', 'p':
+    return 3
+  case 'f', 'h', 'v', 'w', 'y':
+    return 4
+  case 'k':
+    return 5
+  case 'j', 'x':
+    return 8
+  case 'q', 'z':
+    return 10
+  default:
+    return 0
+  }
 }
