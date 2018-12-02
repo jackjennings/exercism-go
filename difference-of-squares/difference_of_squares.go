d3fe667f9d44c8481066ccd3a5fae449a9bfa1ad
Jack Jennings <jack@standard-library.com>
2018-12-02 08:47:26 -0800

    Use math formulas for difference-of-squares to avoid iteration

--- difference-of-squares/difference_of_squares.go
+++ difference-of-squares/difference_of_squares.go
@@ -2,28 +2,17 @@
 package diffsquares
 
 // SquareOfSum returns the square of the sum of the first N natural numbers
-func SquareOfSum(input int) int {
-  var output int
-
-  for i := input; i > 0; i-- {
-    output += i
-  }
-
-  return output * output
+func SquareOfSum(n int) int {
+	sum := (n * (n + 1)) / 2
+	return sum * sum
 }
 
 // SumOfSquares returns the sum of the square of each of the first N natural numbers
-func SumOfSquares(input int) int {
-  var output int
-
-  for i := input; i > 0; i-- {
-    output += i * i
-  }
-
-  return output
+func SumOfSquares(n int) int {
+	return (n * (n + 1) * (2*n + 1)) / 6
 }
 
 // Difference returns the SquareOfSum less the SumOfSquares
 func Difference(input int) int {
-  return SquareOfSum(input) - SumOfSquares(input)
+	return SquareOfSum(input) - SumOfSquares(input)
 }
