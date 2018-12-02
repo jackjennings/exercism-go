32476baa754d98de1a4f17a8d1c1a80e5406b6aa
Jack Jennings <jack@standard-library.com>
2018-12-02 11:08:27 -0800

    Iterate over raw byte sequence in luhn

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -1,15 +1,12 @@
 // Package luhn implements functions for working with the Luhn checksum algorithm
 package luhn
 
-import (
-	"strconv"
-	"strings"
-)
+import "strings"
 
 // Valid checks if the given input passes the Luhn algorithm
 func Valid(input string) bool {
 	var sum int
-	var sequence = strings.Split(strings.Replace(input, " ", "", -1), "")
+	var sequence = strings.Replace(input, " ", "", -1)
 	var length = len(sequence)
 
 	if length == 1 {
@@ -17,13 +14,16 @@ func Valid(input string) bool {
 	}
 
 	for i := length; i > 0; i-- {
-		v, err := strconv.Atoi(sequence[i-1])
+		// Substracting the '0' rune gets the character code offset of subsequent numbers
+		digit := int(sequence[i-1] - '0')
 
-		if err != nil {
+		// Anything outside of 0-9 is some other character
+		if digit > 9 || digit < 0 {
 			return false
 		}
 
-		sum += ((v*(((length-i)%2)+1))-1)%9 + 1
+		// Double every second digit, subtracting 9 for values greater than 9
+		sum += ((digit*(((length-i)%2)+1))-1)%9 + 1
 	}
 
 	return sum%10 == 0
